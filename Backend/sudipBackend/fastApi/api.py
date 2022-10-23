0811253f1ef98169fc493ced7a650a4aa87ee5da
sudips413 <sudips413@gmsil.com>
2022-10-23 22:04:26 +0545

    voice backend connected and updated

--- Backend/sudipBackend/fastApi/api.py
+++ Backend/sudipBackend/fastApi/api.py
@@ -4,11 +4,14 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from uuid import UUID
-import os
+from os.path import splitext
+from nepalimodel.predict import predict_from_speech
+from pydub import AudioSegment
 from pythonfiles.main import get_summary_from_text_file
 from pythonfiles import tokenizer
 from pythonfiles import ranker
 from pydantic import BaseModel
+
 
 
 
@@ -43,7 +46,8 @@ async def create_upload_text(data: text):
     
     
 
-
+#endpoint for fileinput-text
+############################################################################################################
 @app.post("/text")
 async def create_upload_file(text: UploadFile = File(...)):
     ext=text.filename.split('.').pop()
@@ -59,17 +63,28 @@ async def create_upload_file(text: UploadFile = File(...)):
         
     else:
         return {"Summary not found! Please upload a text file"}  
+
+
+#endpoint for audioinput
+##########################################################################################################
       
 @app.post("/audio")
 def create_upload_file(audio: UploadFile = File(...)):    
     ext=audio.filename.split('.').pop()
-    if ext == 'flac':  
+    if ext == 'flac' or ext == 'wav' or ext == 'mp3':  
+        # if(ext=='wav'):
+        #     song = AudioSegment.from_wav(audio.filename)
+        #     song.export("testme.flac",format = "flac")
+            
+        
         file_location = f"static/audio/{audio.filename}{uuid.uuid1()}"
         with open(file_location, "wb+") as file_object:
             file_object.write(audio.file.read())
-        return {"message": "File saved"}
+        transcript=predict_from_speech(file_location)
+        return transcript
+        # return audio
     else:
-        return {"message": "File extension is not flac"}
+        return {"Please upload a flac or wav or mp3 file! upload failed"}
 
         
 
