17e827286244db4c0f7b7cf06975bf45afb337ec
juju2181 <anishshilpakar8@gmail.com>
2023-02-15 15:23:38 +0000

    Modified trainer by removing the error rates and included only train and validation loss. This helped the training process to use GPU and training was much faster

--- Training/ASR/asr_cnn_resnet/trainer.py
+++ Training/ASR/asr_cnn_resnet/trainer.py
@@ -126,8 +126,13 @@ def calculateBatchErrorRates(output,target,start,end,cer,wer,isValidation=False)
     """
     input_len = np.ones(output.shape[0]) * output.shape[1]
     # Decode the output using beam search and CTC to get  the required logits
-    decoded_indices = K.ctc_decode(output, input_length=input_len,
-                            greedy=False, beam_width=100)[0][0]
+    # decoded_indices = K.ctc_decode(output, input_length=input_len,
+    #                         greedy=False, beam_width=100)[0][0]
+    outputs = tf.nn.ctc_greedy_decoder(
+    logits,
+    seq_lens,
+    blank_index=1)
+    decoded_indices = K.ctc_greedy_decoder(output, sequence_length=input_len)[0][0]
     
     # Remove the padding token from batchified target texts
     target_indices = [sent[sent != 0].tolist() for sent in target]
@@ -163,7 +168,7 @@ def train_model(model, optimizer, train_wavs, train_texts, validation_wavs, vali
 
     with tf.device(device_name):
         # Definition of checkpoint
-        checkpoint_dir = '/content/drive/MyDrive/training-checkpoints'
+        checkpoint_dir = './training-checkpoints'
         checkpoint_prefix = os.path.join(checkpoint_dir,'ckpt')
         checkpoint = tf.train.Checkpoint(optimizer =optimizer, model = model)
         # restore latest checkpoint
@@ -176,21 +181,21 @@ def train_model(model, optimizer, train_wavs, train_texts, validation_wavs, vali
         # These will be the final results to be returned
         train_losses = []
         validation_losses = []
-        train_CERs = []
-        train_WERs = []
-        validation_CERs = []
-        validation_WERs = []
+        # train_CERs = []
+        # train_WERs = []
+        # validation_CERs = []
+        # validation_WERs = []
         for e in range(start_epoch, end_epoch):
             epoch = e+1
             start_time = time.time()
             len_train = len(train_wavs)
             len_validation = len(validation_wavs)
             training_loss = 0
-            training_CER = 0
-            training_WER = 0
+            # training_CER = 0
+            # training_WER = 0
             validation_loss = 0
-            validation_CER = 0
-            validation_WER = 0
+            # validation_CER = 0
+            # validation_WER = 0
             train_batch_count = 0
             validation_batch_count = 0
             # Training Steps
@@ -216,7 +221,7 @@ def train_model(model, optimizer, train_wavs, train_texts, validation_wavs, vali
 
                 training_loss += np.average(loss.numpy())
                 train_batch_count += 1
-                training_CER, training_WER = calculateBatchErrorRates(output,target,start,end,training_CER,training_WER)
+                # training_CER, training_WER = calculateBatchErrorRates(output,target,start,end,training_CER,training_WER)
 
 
             # Validation Step
@@ -239,45 +244,47 @@ def train_model(model, optimizer, train_wavs, train_texts, validation_wavs, vali
 
                 validation_loss += np.average(loss.numpy())
                 validation_batch_count += 1
-                validation_CER, validation_WER, predicted_labels, actual_labels = calculateBatchErrorRates(output,target,start,end,validation_CER,validation_WER,isValidation=True)
-            print(f"\nPred: {predicted_labels[0].split()}")
-            print(f"Actual: {actual_labels[0].split()}\n")
+                # validation_CER, validation_WER, predicted_labels, actual_labels = calculateBatchErrorRates(output,target,start,end,validation_CER,validation_WER,isValidation=True)
+            # print(f"\nPred: {predicted_labels[0].split()}")
+            # print(f"Actual: {actual_labels[0].split()}\n")
             # Average the results
             # losses
             training_loss /= train_batch_count
             validation_loss /= validation_batch_count
-            # cers
-            training_CER /= train_batch_count
-            validation_CER /= validation_batch_count
-            # wers 
-            training_WER /= train_batch_count 
-            validation_WER /= validation_batch_count
+            # # cers
+            # training_CER /= train_batch_count
+            # validation_CER /= validation_batch_count
+            # # wers 
+            # training_WER /= train_batch_count 
+            # validation_WER /= validation_batch_count
 
             # Append the results 
             train_losses.append(training_loss)
-            train_CERs.append(training_CER)
-            train_WERs.append(training_WER)
+            # train_CERs.append(training_CER)
+            # train_WERs.append(training_WER)
             validation_losses.append(validation_loss)
-            validation_CERs.append(validation_CER)
-            validation_WERs.append(validation_WER)
+            # validation_CERs.append(validation_CER)
+            # validation_WERs.append(validation_WER)
             
-            rec = f"Epoch: {epoch}, Train Loss: {training_loss:.2f}, Validation Loss: {validation_loss:.2f}, Train CER: {(training_CER*100):.2f}, Validation CER: {(validation_CER*100):.2f}, Train WER: {(training_WER*100):.2f}, Validation WER: {(validation_WER*100):.2f} in {(time.time() - start_time):.2f} secs\n"
+            # rec = f"Epoch: {epoch}, Train Loss: {training_loss:.2f}, Validation Loss: {validation_loss:.2f}, Train CER: {(training_CER*100):.2f}, Validation CER: {(validation_CER*100):.2f}, Train WER: {(training_WER*100):.2f}, Validation WER: {(validation_WER*100):.2f} in {(time.time() - start_time):.2f} secs\n"
+
+            rec = f"Epoch: {epoch}, Train Loss: {training_loss:.2f}, Validation Loss: {validation_loss:.2f} in {(time.time() - start_time):.2f} secs\n"
 
             print(rec)
-
-            print(f"Now saving checkpoint for epoch {epoch}")
-            checkpoint.save(checkpoint_prefix) 
-            print('Checkpoint Saved')
+            if epoch % 10 == 0:
+                print(f"Now saving checkpoint for epoch {epoch}")
+                checkpoint.save(checkpoint_prefix) 
+                print('Checkpoint Saved')
 
 
         result = {
             'epochs': list(range(start_epoch+1,end_epoch+1)),
             'train_loss': train_losses,
             'validation_loss': validation_losses,
-            'train_cer': train_CERs,
-            'validation_cer': validation_CERs,
-            'train_wer': train_WERs,
-            'validation_wer': validation_WERs
+            # 'train_cer': train_CERs,
+            # 'validation_cer': validation_CERs,
+            # 'train_wer': train_WERs,
+            # 'validation_wer': validation_WERs
         }
     
     return model, result
@@ -295,7 +302,7 @@ def train_model(model, optimizer, train_wavs, train_texts, validation_wavs, vali
 #     train_texts = texts_df["label"].tolist()
 #     return train_wavs, train_texts
 
-def load_data_with_mfcc(wavs_dir, texts_dir):
+def load_data_with_mfcc(texts_dir):
     texts_df = pd.read_csv(texts_dir)
     train_texts = texts_df["label"].to_list()
     train_wavs = texts_df["mfcc"].to_list()
@@ -304,17 +311,16 @@ def load_data_with_mfcc(wavs_dir, texts_dir):
 
 def update_csv(result):
     print("Now updating csv file")
-    original_csv_path = "/content/drive/MyDrive/results_20k.csv"
+    original_csv_path = "./results_new_data.csv"
     original_df = pd.read_csv(original_csv_path)
     df_temp = pd.DataFrame(result)
     df_temp.rename(columns={"epochs":"epoch"},inplace=True)
-    df_temp["train_cer"] = df_temp["train_cer"]*100
-    df_temp["validation_cer"] = df_temp["validation_cer"]*100
-    df_temp["train_wer"] = df_temp["train_wer"]*100
-    df_temp["validation_wer"] = df_temp["validation_wer"]*100
+    # df_temp["train_cer"] = df_temp["train_cer"]*100
+    # df_temp["validation_cer"] = df_temp["validation_cer"]*100
+    # df_temp["train_wer"] = df_temp["train_wer"]*100
+    # df_temp["validation_wer"] = df_temp["validation_wer"]*100
     df_final = pd.concat([original_df,df_temp],ignore_index=True)
     print(f"Updated results dataframe now have {df_final.shape[0]} rows")
-    df_final.to_csv("/content/drive/MyDrive/new_results.csv",index=False)
     df_final.to_csv(original_csv_path,index=False)
     print("Updated CSV saved")
 
@@ -332,41 +338,11 @@ if __name__ == "__main__":
     # Defintion of the optimizer
     optimizer = tf.keras.optimizers.Adam()
 
-    # t1 = time.time()
-    # # Load the data
-    # print("Loading data.....")
-    # train_wavs, train_texts = load_data(
-    #     wavs_dir="/content/audio", texts_dir="/content/drive/MyDrive/Automatic-Nepali-Speech-Recognition-and-Summarizer/ASR/data_asr/transcript_asr/new_transcript_for_asr_complete.csv")
-    # t2 = time.time()
-    # print(f"Data loaded \u2705 \u2705 \u2705 \u2705\nAnd It took {t2-t1} seconds\n")
-
-    # """
-    # To replicate the results give the argument as text_dir="dataset/transcriptions(sampled)/file_speaker_text(orignally_trained).csv".
-    # Get all of the wavs files from https://openslr.org/54/, put them in a single directory, and give that directory as argument for wavs_dir.
-    # """
-    
-    # # Clean the audio file by removing the silent gaps from the both ends the audio file
-    # t3 = time.time()
-    # print("Cleaning the audio files.....")
-    # train_wavs = [clean_single_wav(wav) for wav in train_wavs]
-    # t4 = time.time()
-    # print(f"Audio files cleaned \u2705 \u2705 \u2705 \u2705\nAnd It took {t4-t3} seconds\n")
-
-    # # Generate mfcc features for the audio files
-    # t5 = time.time()
-    # print("Generating mfcc features.....")
-    # train_wavs = [gen_mfcc(wav) for wav in train_wavs]
-    # t6 = time.time()
-    # print(f"MFCC features generated \u2705 \u2705 \u2705 \u2705\nAnd It took {t6-t5} seconds\n")
-
-    # print(f"Total Time for Loading Data: {(t2-t1)+(t4-t3)+(t6-t5)} seconds")
-
     #load data along with mfcc
     t1 = time.time()
     # Load the data
     print("Loading data.....")
-    train_wavs, train_texts = load_data_with_mfcc(
-        wavs_dir="/content/audio/audio", texts_dir="/content/data_with_mfcc_0_20000.csv")
+    train_wavs, train_texts = load_data_with_mfcc(texts_dir="~/manualpartition/teamSaransha/data/data_cnn/data_first_40k.csv")
     t2 = time.time()
     print(f"Data loaded with mfcc \u2705 \u2705 \u2705 \u2705\nAnd It took {t2-t1} seconds\n")
 
@@ -374,22 +350,29 @@ if __name__ == "__main__":
     """
     Originally the data was split in the 95% train and 5% test set; With total of 148K (audio,text) pairs.
     """
+    t3 = time.time()
+    print("Splitting data.....")
     train_wavs, test_wavs, train_texts, test_texts = train_test_split(
         train_wavs, train_texts, test_size=0.2,shuffle=False, random_state=None)
-
+    t4 = time.time()
+    print(f"Data splitted \u2705 \u2705 \u2705 \u2705\nAnd It took {t4-t3} seconds\n")
     # Train the model
     """
     Originally the model was trained for 58 epochs; With a batch size of 50.
     """
     # loading checkpoint
     # model = load_model('/content/drive/MyDrive/Training_Checkpoints/checkpoint_50.h5')
-
+    t5 = time.time()
+    print("Training Model.....")
     model_trained, result = train_model(model, optimizer, train_wavs, train_texts,
-                test_wavs, test_texts, start_epoch=40,end_epoch=42, batch_size=100,restore_checkpoint=True)
-    # model_trained.save('/content/drive/MyDrive/Trained_Models/model1_20000.h5')
-    print('Model Trained')
+                test_wavs, test_texts, start_epoch=0,end_epoch=50, batch_size=64,restore_checkpoint=True)
+    model_trained.save('./model1.h5')
+    t6 = time.time()
+    print(f"Model Trained and Saved \u2705 \u2705 \u2705 \u2705\nAnd It took {t6-t5} seconds\n")
 
     update_csv(result)
+    print('Results updated \u2705 \u2705 \u2705 \u2705\n')
+    print(f"Total time taken: {time.time() - t1} seconds")
 
 
 
